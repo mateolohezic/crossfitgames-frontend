2039e0f656dd83c974f402bc0242873db8449362
Gonzalo Morales <gonzalomorales1001@gmail.com>
2023-10-31 15:50:33 -0300

    copiado navbar de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import './App.css'
 import Home from './Pages/Home/Home';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
     <>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home/>} />
     </Routes>
